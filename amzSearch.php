0984d1619d9dd28f1856cd2bad0d536325a9cc72
jhlz <jl@lavalized.de>
2016-02-17 12:45:25 +0100

    new files

---
+++ amzSearch.php
@@ -0,0 +1,75 @@
+<?
+/****  amazon search **/
+require_once 'res/simplexml.class.php';
+// include('res/url_get_contents.php'); //for older php versions
+
+function searchAmazon($country, $artistName, $albumName){
+if ($country == "DE"){$country="de";}	
+$accessKeyId = ""; // aws id
+$secretKey = ""; // aws secret key
+$associateTag = ""; // affiliate tag 
+
+if ($albumName == ""){		
+		$Keywords = $artistName;	
+	}else{
+		$Keywords = $artistName." ".$albumName;
+	}
+
+$Timestamp = gmdate("Y-m-d\TH:i:s\Z"); 
+$ResponseGroup = "ItemAttributes";
+$ResponseGroup = str_replace(",", "%2C", $ResponseGroup);
+$Keywords = str_replace(" ", "+", $Keywords);
+$Keywords = str_replace("&", " ", $Keywords);	
+
+$params = "AWSAccessKeyId=$accessKeyId&
+AssociateTag=$associateTag&
+Keywords=$Keywords&
+Operation=ItemSearch&
+SearchIndex=MP3Downloads&
+ItemPage=1&
+ResponseGroup=$ResponseGroup&
+Service=AWSECommerceService&
+Timestamp=$Timestamp&
+Version=2013-08-01";
+
+$BaseUrl = "http://webservices.amazon.".$country."/onca/xml?";
+$params= $BaseUrl . $params;
+$params = str_replace("%7E", "~", $params);
+$params = preg_replace('/[\r\n\" "]/', '', $params);
+$params = preg_replace('/["  "]+/', ' ', $params);
+$paramsArray = explode( '?', $params, 2);
+$Parameters = explode("&", $paramsArray[1]);
+asort($Parameters);
+$NewString = implode("&", $Parameters);
+$NewString = rawurlencode($NewString);
+$NewString = str_replace("~", "%7E", $NewString);
+$NewString = str_replace("%3D", "=", $NewString);
+$NewString = str_replace("%26", "&", $NewString);
+$NewString = str_replace("%FA", "%C3%BA", $NewString); // ú
+$NewString = str_replace("%E9", "%C3%A9", $NewString); // é   
+$NewString = str_replace("%F6", "%C3%B6", $NewString);  // ö
+$NewString = str_replace("%E1", "%C3%A1", $NewString);  // á
+$NewString = str_replace("%F3", "%C3%B3", $NewString);  // ó
+$NewString = str_replace("%FC", "%C3%BC", $NewString);  // ü
+$NewString = str_replace("%E4", "%C3%A4", $NewString);  // ä
+$url = parse_url($paramsArray[0]);
+$PrependString = "GET\n" .$url['host']. "\n" .$url['path']. "\n" .$NewString;
+$Signature= base64_encode(hash_hmac('sha256', $PrependString, $secretKey, true)); 
+$Signature = rawurlencode($Signature);
+$SignedRequest = $paramsArray[0]. "?" . $NewString . "&Signature=" .$Signature;
+
+
+$file = $SignedRequest;
+$sxml = new simplexml;
+$dataa = $sxml->xml_load_file($file);
+$resCount = (int) $dataa->Items->TotalResults;
+//$amz_err = $dataa->Items->Request->Errors->Error->Code;
+$amazon_url = $dataa->Items->MoreSearchResultsUrl;
+
+if ($resCount == 0){$res="empty";}											
+elseif ($resCount > 0){$res= array($amazon_url);}	
+else {$res="error";}
+ 
+ return $res;
+
+ }
---
+++ test.php
@@ -0,0 +1,16 @@
+<?php/*** Test for Amazon search**/
+require_once "amzSearch.php";	
+	if (isset($_POST['firstname-amz']) AND isset($_POST['lastname-amz']))
+	{
+			$res = searchAmazon("DE",$_POST['firstname-amz']." ".$_POST['lastname-amz'],$_POST['album-amz']);			$resA= $res['0'];
+						$found = strpos($resA, "http");					if ($found !== false){				echo "<a href='".$resA."'>".$resA."</a>";			}			else {echo $resA;}
+	}
+		
+?>	
+<h2>Amazon Search Test</h2>
+<form action='test.php' method='post'>
+<p>firstname: <input type='text' name='firstname-amz' /></p>
+<p>lastname: <input type='text' name='lastname-amz' /></p>
+<p>album: <input type='text' name='album-amz' /></p>
+<p><input type='submit' /></p></form>
+
